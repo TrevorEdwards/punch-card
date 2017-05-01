e4ad4150f96f0d0d12bed7115ba009ec926e0660
Trevor Edwards <tre23@cornell.edu>
2017-05-01 13:25:05 -0400

    more commit

---
+++ filler.sh
@@ -0,0 +1,1 @@
+echo filler >> filler.txt
--- punchcard.py
+++ punchcard.py
@@ -1,4 +1,4 @@
-import subprocess
+import subprocess, time, git
 
 
 def main():
@@ -27,25 +27,34 @@ def main():
     for i in range(7):
         date = dates[i]
         for j in range(24):
-            time = times[j]
+            time_clock = times[j]
             for entry in lines:
-                if date in entry and time in entry:
+                if date in entry and time_clock in entry:
                     satisfied[i][j] = True
 
     for i in range(7):
         date = dates[i]
         for j in range(24):
-            time = times[j]
+            time_clock = times[j]
             if not satisfied[i][j]:
-                print(date + ' ' + time)
+                print(date + ' ' + time_clock)
+                bs_commit(date,time_clock)
     
-def bs_commit(day, time):
-    fake_echo = 'echo filler >> filler.txt'.split(' ')
+def bs_commit(day, time_clock):
+    sleep_time = 0.1
+    fake_echo = ['bash','filler.sh']
     do_add = 'git add filler.txt'.split(' ')
-    do_commit = 'git commit -m "timestamp filler" --date "' + day + ' Apr 1' + time + ':00:00 2017 -0400"'.split(' ')
-    subprocess.Popen(fake_echo, stdout=subprocess.PIPE)
+    do_commit = ['git', 'commit', '-m' ,'timestamp filler' ,'--date', str(day) + ' Apr 1' + str(time_clock) + '00:00 2017 -0400' ]
+    print(do_commit)
+    print("doing filler")
+    subprocess.call(fake_echo, stdout=subprocess.PIPE)
+    time.sleep(sleep_time)
+    print("doing add")
     subprocess.Popen(do_add, stdout=subprocess.PIPE)
+    time.sleep(sleep_time)
+    print("doing commit")
     subprocess.Popen(do_commit, stdout=subprocess.PIPE)
+    time.sleep(sleep_time)
 
 main()
 
